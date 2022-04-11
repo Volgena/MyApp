d609b4c2fd8ff5cd9bc2e8ba31bad44b0837f8ea
Asus <ozdanobich1.21@gmail.com>
2022-04-11 20:47:41 +0300

    Hello

---
+++ src/main/java/MyGit.java
@@ -0,0 +1,12 @@
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello new day");
+        System.out.println("Hello happy new day");
+        System.out.println("Hello new day");
+        System.out.println("Hello happy new day");
+        int a=8;
+        int b=15;
+
+    }
+}
